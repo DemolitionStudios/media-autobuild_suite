eeb2ab86c3c7472b35fcafdd13e8f9232397d71a
krylovgeorgii <krylov.georgii@gmail.com>
2018-04-20 00:30:11 +0300

    add \build

--- move_dll.bat
+++ move_dll.bat
@@ -1,2 +1,3 @@
-copy build\mpv-git\build\mpv-1.dll local64\mpv_root\mpv.dll
-bin\cv2pdb -C local64\mpv_root\mpv.dll
+mkdir local64\mpv_root\build
+copy build\mpv-git\build\mpv-1.dll local64\mpv_root\build\mpv.dll
+bin\cv2pdb -C local64\mpv_root\build\mpv.dll
